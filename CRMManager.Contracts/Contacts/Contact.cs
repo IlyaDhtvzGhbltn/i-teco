43c3c4330f8d3892ffaaaa094370ca5ca19e986f
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2022-09-18 18:29:49 +0300

    contracts added

---
+++ CRMManager.Contracts/Contacts/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CRMManager.Contracts.Contacts
+{
+    public class Contact
+    {
+        public Guid ID { get; set; }
+        public Guid ContactFormID { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(Guid ID, bool isActive, DateTime registrationDate)
+        {
+            this.ID = ID;
+            this.IsActive = isActive;
+            this.RegistrationDate = registrationDate;
+        }
+    }
+}
---
+++ CRMManager.Contracts/Contacts/NewContact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRMManager.Contracts.Contacts
+{
+    public class NewContact
+    {
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
