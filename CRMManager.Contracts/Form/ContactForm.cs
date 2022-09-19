f522dabbe1d17beb57af95368db33deb9b2ddf90
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2022-09-19 17:53:28 +0300

    forms contract added

---
+++ CRMManager.Contracts/Form/ContactForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRMManager.Contracts.Form
+{
+    public class ContactForm
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
